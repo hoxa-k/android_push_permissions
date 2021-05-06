5c2c93fc79c876622a6df7012bb0a71f962d849e
Kulikova Natalia <kulikova_n@tass.ru>
2021-05-06 16:55:09 +0300

    fix null safety

--- lib/android_push_permissions.dart
+++ lib/android_push_permissions.dart
@@ -7,7 +7,7 @@ class AndroidPushPermissions {
   static const MethodChannel _channel =
       const MethodChannel('android_push_permissions');
 
-  static Future<bool> isEnabled({String channelId}) async {
+  static Future<bool> isEnabled({String? channelId}) async {
     final bool result = await _channel.invokeMethod('isEnabled', <String, dynamic>{
             'channelId': channelId,
            });
