5d9527c641ad4d150993242bb2afa7d3c6a49d50
Kulikova Natalia <kulikova_n@tass.ru>
2021-05-06 17:53:52 +0300

    fix code with lint

--- lib/android_push_permissions.dart
+++ lib/android_push_permissions.dart
@@ -4,11 +4,11 @@ import 'package:flutter/services.dart';
 
 class AndroidPushPermissions {
   static const MethodChannel _channel =
-      const MethodChannel('android_push_permissions');
+      MethodChannel('android_push_permissions');
 
-  static Future<bool> isEnabled({String? channelId}) async {
-    final bool result =
-        await _channel.invokeMethod('isEnabled', <String, dynamic>{
+  static Future<bool?> isEnabled({String? channelId}) async {
+    final result =
+        await _channel.invokeMethod<bool>('isEnabled', <String, dynamic>{
       'channelId': channelId,
     });
     return result;
