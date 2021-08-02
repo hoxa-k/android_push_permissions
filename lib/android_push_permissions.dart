1ab47475114e0321e293413768ebc5ca235e9d5b
Gennady Kaurov <34021723+TheRealMeekey@users.noreply.github.com>
2021-08-02 10:11:03 +0300

    add channel.setShowBadge(false); (#1)
    * add channel.setShowBadge(false);
    
    * add channel.setShowBadge(false);
    
    * add channel.setShowBadge(false);
    
    Co-authored-by: kaiurov_g <kaiurov_g@tass.ru>

--- lib/android_push_permissions.dart
+++ lib/android_push_permissions.dart
@@ -6,10 +6,12 @@ class AndroidPushPermissions {
   static const MethodChannel _channel =
       MethodChannel('android_push_permissions');
 
-  static Future<bool?> isEnabled({String? channelId}) async {
+  static Future<bool?> isEnabled(
+      {bool isBadge = false, String? channelId}) async {
     final result =
         await _channel.invokeMethod<bool>('isEnabled', <String, dynamic>{
       'channelId': channelId,
+      'isBadge': isBadge,
     });
     return result;
   }
